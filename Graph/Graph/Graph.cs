02e818049b1e7287721c17a051616249cde438e2
DESKTOP-ASQ0J34\MI <skyland7494@mail.ru>
2017-09-21 15:51:40 +0300

    Graph class

---
+++ Graph/Graph/Graph.cs
@@ -0,0 +1,14 @@
+﻿namespace Graph
+{
+    public class Graph
+    {
+        public int[] Heads { get; set; }
+        public int[] Tails { get; set; }
+
+        public Graph(int[] heads, int[] tails)
+        {
+            Heads = heads;
+            Tails = tails;
+        }
+    }
+}
---
+++ Graph/Graph/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Graph
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}

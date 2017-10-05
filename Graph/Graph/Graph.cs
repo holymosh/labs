6c29105e9a0706de42a85a0ed5985dc280569969
DESKTOP-ASQ0J34\MI <skyland7494@mail.ru>
2017-10-05 15:49:43 +0300

    added week buttons to scheduler menu.

--- Graph/Graph/Graph.cs
+++ Graph/Graph/Graph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,11 +12,18 @@ namespace Graph
         public IList<int> Tails { get; set; } // J
         public IList<int> HeadsList { get; set; } //головы списков , массив H ,размерность n
         public IList<int> References { get; set; } // ссылки элементов друг на друга, массив L , размерность m
+        public IList<int> Colors { get; set; }
+        public IList<KeyValuePair<int,int>> HeadsTails { get; set; } // IJ
 
         public Graph(int[] heads, int[] tails)
         {
             Heads = new List<int>(heads);
             Tails = new List<int>(tails);
+            HeadsTails = new List<KeyValuePair<int, int>>();
+            for (int i = 0; i < Heads.Count; i++)
+            {
+                HeadsTails.Add(new KeyValuePair<int, int>(Heads[i],Tails[i]));
+            }
             CreateArcList();
         }
 
@@ -23,6 +31,7 @@ namespace Graph
         {
             Heads.Add(head);
             Tails.Add(tail);
+            HeadsTails.Add(new KeyValuePair<int, int>(head,tail));
             CreateArcList();
         }
 
@@ -55,12 +64,25 @@ namespace Graph
         {
             var count = Heads.Concat(Tails).Max() + 1;
             HeadsList = Enumerable.Repeat(-1, count).ToList();
+            Colors = Enumerable.Repeat(-1,count).ToList();
             References = new List<int>(Heads.Count);
             for (int k = 0; k < Heads.Count; k++)
             {
                 var i = Heads[k];
                 References.Add(HeadsList[i]);
                 HeadsList[i] = k;
+            }
+        }
+
+        public void DepthFirstSearch(int vertex, int currentComponent)
+        {
+            Colors[vertex] = currentComponent;
+            for (int index = HeadsList[vertex] = 0; !index.Equals(-1); index = References[index])
+            {
+                if (Colors[HeadsTails[index].Value].Equals(-1))
+                {
+                    DepthFirstSearch(HeadsTails[index].Value , currentComponent);
+                }
             }
         }
     }
